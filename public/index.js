6c8d32d4046b1ff9d210d3794b368ae98314d9ae
Daiana <dauipoma@gmail.com>
2023-03-24 23:42:07 +0100

    Level 2

--- public/index.js
+++ public/index.js
@@ -3,7 +3,9 @@ const nextJokeBtn = document.querySelector('.btn');
 const jokeDisplay = document.querySelector('.joke');
 const ratingDisplay = document.querySelector('.rating');
 const ratingButtons = document.querySelectorAll('.score');
-const JOKE_API_URL = 'https://icanhazdadjoke.com/';
+const weatherDisplay = document.querySelector('.weather');
+const WEATHER_API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&current_weather=true';
+const JOKE_API_URLS = ['https://icanhazdadjoke.com/', 'https://api.chucknorris.io/jokes/random'];
 // Reports
 const reportJokes = [];
 // Data of current joke
@@ -30,9 +32,10 @@ const resetRating = () => {
     ratingButtons.forEach(btn => btn.checked = false);
 };
 const showJoke = async () => {
+    const random = Math.floor(Math.random() * (2 - 0) + 0);
     try {
-        const { joke } = await getJoke(JOKE_API_URL);
-        jokeDisplay.textContent = joke;
+        const joke = await getJoke(JOKE_API_URLS[random]);
+        jokeDisplay.textContent = joke.joke || joke.value || 'Try Again...';
         ratingDisplay.style.display = 'flex';
     }
     catch (error) {
@@ -48,4 +51,11 @@ nextJokeBtn.addEventListener('click', () => {
     if (currentJoke.score != 0) {
         addJokeReport(jokeDisplay.textContent, currentJoke.score);
     }
+});
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(WEATHER_API_URL)
+        .then(res => res.json())
+        .then(data => {
+        weatherDisplay.textContent = `${data.current_weather.temperature} °C`;
+    });
 });
